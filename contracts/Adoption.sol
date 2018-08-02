d590def9ed998853a5337321940c74b8a8fc2ecd
Smit Rajput <smitrajputtd@gmail.com>
2018-08-02 21:05:25 +0530

    Add solidity tests

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -4,15 +4,15 @@ contract Adoption {
     address[16] public adopters;
 
     // Adopting a pet
-function adopt(uint petId) public returns (uint) {
-  require(petId >= 0 && petId <= 15);
+    function adopt(uint petId) public returns (uint) {
+      require(petId >= 0 && petId <= 15);
 
-  adopters[petId] = msg.sender;
+      adopters[petId] = msg.sender;
 
-  return petId;
-}
+      return petId;
+    }
 
-    function getAdopters() view returns(address[16]) {
+    function getAdopters() public view returns(address[16]) {
         return adopters;
     }
 }
